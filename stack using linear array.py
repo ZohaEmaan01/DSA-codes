609868e211dc3338cb0ff18bd8c20f76e41d8261
Zoha Emaan <163720799+ZohaEmaan01@users.noreply.github.com>
2024-06-04 17:39:06 +0500

    Stack using Linear Array

---
+++ stack using linear array.py
@@ -0,0 +1,49 @@
+class Stack:
+    def __init__(self,size):
+        self.size = size
+        self.arr = [None] * size
+        self.top = -1
+
+    def push(self,val):
+        if self.top == self.size - 1:
+            raise Exception('stack overflow')
+        self.top += 1
+        self.arr[self.top] = val
+
+    def pop(self):
+        if self.top == -1:
+            raise Exception('stack is empty')
+        temp = self.arr[self.top]
+        self.top -= 1
+        return temp
+
+    def peek(self):
+        if self.top == -1:
+            raise Exception('stack is empty')
+        return self.arr[self.top]
+
+    def is_empty(self):
+        return self.top == -1
+
+    def is_full(self):
+        return self.top == self.size - 1
+
+    def __str__(self):
+        return str(self.arr[:self.top+1])
+
+def main():
+    stack = Stack(5)
+
+    print(stack.is_empty())   # True
+    print(stack.is_full())    # False
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    print(stack)
+
+    print(stack.pop())
+
+    print(stack)
+main()
